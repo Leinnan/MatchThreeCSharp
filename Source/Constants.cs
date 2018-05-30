cb67ba86a1a26e5399289c5ee8440695e9db5235
Leinnan <rybopiotr@gmail.com>
2018-05-30 17:29:09 +0200

    Use Board class

--- Source/Constants.cs
+++ Source/Constants.cs
@@ -3,11 +3,19 @@ namespace MonoGameCore
     public static class Constants
     {
         public const string GAME_NAME = "MatchThree";
-        public const int BOARD_SIZE = 10;
-        public const int FIELD_SIZE = 30;
-        public const bool DEBUG_INFO = false;
+        public const int BOARD_SIZE = 12;
+        public const int FIELD_SIZE = 32;
         public const int SCREEN_WIDTH = 480;
         public const int SCREEN_HEIGHT = 800;
+        public const int SYMBOLS_TYPES_AMOUNT = 8;
+
+        public enum GAME_SUBSTATE {
+            IDLE,
+            SWAPING,
+            WINING,
+            SWAPING_BACK,
+            DROPPING
+        }
 
         public const string INGAME_BG = "gfx/bg";
         public static readonly string[] SYMBOLS_PATH = {
--- Source/GameState.cs
+++ Source/GameState.cs
@@ -1,7 +1,6 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using System;
@@ -12,8 +11,8 @@ namespace MonoGameCore {
 
         private SpriteFont font;
         private Texture2D background;
-        public Board boardGfx;
-        private Texture2D[] m_symbols;
+        public Board m_board;
+        public int m_score;
 
         public GameState(){}
 
@@ -53,15 +52,10 @@ namespace MonoGameCore {
         
         public override void OnLoad(ContentManager content, GraphicsDevice graphics)
         {
+            Console.WriteLine("OnLoad"); 
             font = content.Load<SpriteFont>("Font"); // Use the name of your sprite font file here instead of 'Score'.
             background = content.Load<Texture2D>(Constants.INGAME_BG);
-            boardGfx = new Board(graphics);
-            m_symbols = new Texture2D[16];
-
-            for(int i = 0; i < 16; i++)
-            {
-                m_symbols[i] = content.Load<Texture2D>(Constants.SYMBOLS_PATH[i]);
-            }
+            m_board = new Board(content, graphics);
         }
         public override void OnUnload(ContentManager content, GraphicsDevice graphics)
         {
@@ -69,29 +63,10 @@ namespace MonoGameCore {
         }
         public override void OnDraw(ref SpriteBatch spriteBatch)
         {
+            Console.WriteLine("OnDraw"); 
             spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
-            boardGfx.Draw(ref spriteBatch);
-            for(int y = 0; y < 12; y++)
-                for(int x = 0; x < 12; x++)
-                    spriteBatch.Draw(m_symbols[((x+y)+16)%500%16], new Vector2((480-394)/2+x*32, 50+(32*y)), Color.White);
-            spriteBatch.DrawString(font, "Score", new Vector2(10, 10), Color.Black);
+            m_board.Draw(ref spriteBatch);
+            spriteBatch.DrawString(font, "Score: " + m_score.ToString(), new Vector2(10, 10), Color.Black);
         }  
-        
-        public struct Board
-        {
-            public Board(GraphicsDevice graphics)
-            {
-                m_background = new FilledRectangle(
-                    new Vector2((480-394)/2,50), // position
-                    new Vector2(394,394), // size
-                    new Color(0,0,0,200));
-                m_background.GenerateTexture(graphics);
-            }
-            public void Draw(ref SpriteBatch spriteBatch)
-            {
-                m_background.Draw(ref spriteBatch);
-            }
-            public FilledRectangle m_background;
-        }   
     }
 }
