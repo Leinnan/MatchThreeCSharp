0aff0482cb924dad78beafd91011962889b2ccc4
Leinnan <rybopiotr@gmail.com>
2018-05-31 13:29:41 +0200

    Swaping symbols

--- Source/GameState.cs
+++ Source/GameState.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -22,6 +23,7 @@ namespace MonoGameCore {
         public Board m_board;
         public int m_score;
         private Substate m_currentSubstate = Substate.IDLE;
+        SoundEffect m_swipeEffect;
         
         public GameState(){}
 
@@ -37,7 +39,7 @@ namespace MonoGameCore {
         }
         public override void OnUpdate(GameTime gameTime)
         {
-            Console.WriteLine("OnUpdate"); 
+            //Console.WriteLine("OnUpdate"); 
             switch(m_currentSubstate)
             {
                 case Substate.IDLE:
@@ -71,6 +73,33 @@ namespace MonoGameCore {
             }
         }
 
+        public override void OnMouseReleased (Vector2 mousePos, Vector2 movement) 
+        {
+            for(int i = 0; i < Constants.BOARD_SIZE; i++)
+            {
+                for(int j = 0; j < Constants.BOARD_SIZE; j++)
+                {
+                    if(m_board.GetSymbolAtIndex(i,j).IsTouched(mousePos))
+                    {
+                        if(m_board.IsAnySymbolSelected())
+                        {
+                            var selectedSymbolPos = m_board.GetSelectedSymbolPos();
+                            bool canSwap = m_board.CanSwapWithSelectedSymbol(new Vector2(i,j));
+                            m_board.StopSymbolSelection();
+                            if(canSwap)
+                            {
+                                m_board.SwapSymbols(selectedSymbolPos, new Vector2(i,j));
+                                m_swipeEffect.Play();
+                                SwitchState(Substate.SWAPING);
+                            }
+                        }
+                        else
+                            m_board.SelectSymbolAtIndex(i,j);
+                        Console.WriteLine("Symbol o nr [" + i.ToString() + "," + j.ToString() + "]\n\n");
+                    }
+                }
+            }
+        }
         
         public override void OnLoad(ContentManager content, GraphicsDevice graphics)
         {
@@ -78,6 +107,7 @@ namespace MonoGameCore {
             font = content.Load<SpriteFont>("Font"); // Use the name of your sprite font file here instead of 'Score'.
             background = content.Load<Texture2D>(Constants.INGAME_BG);
             m_board = new Board(content, graphics);
+            m_swipeEffect = content.Load<SoundEffect>(Constants.SWIPE_SOUND);
         }
         public override void OnUnload(ContentManager content, GraphicsDevice graphics)
         {
@@ -85,7 +115,7 @@ namespace MonoGameCore {
         }
         public override void OnDraw(ref SpriteBatch spriteBatch)
         {
-            Console.WriteLine("OnDraw"); 
+            //Console.WriteLine("OnDraw"); 
             spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
             m_board.Draw(ref spriteBatch);
             spriteBatch.DrawString(font, "Score: " + m_score.ToString(), new Vector2(10, 10), Color.Black);
